4d96275c207cb8354eff811b1de31e4616e71c94
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2024-12-01 01:47:02 +0600

    initial commit

---
+++ nvim/lua/user/plugins/dashboard-nvim.lua
@@ -0,0 +1,34 @@
+require('dashboard').setup {
+  theme = 'doom',
+  config = {
+    header = {
+          "",
+          "",
+          "",
+          " ███████╗███╗   ██╗██╗ ██████╗ ███╗   ███╗ █████╗ ",
+          " ██╔════╝████╗  ██║██║██╔════╝ ████╗ ████║██╔══██╗",
+          " █████╗  ██╔██╗ ██║██║██║  ███╗██╔████╔██║███████║",
+          " ██╔══╝  ██║╚██╗██║██║██║   ██║██║╚██╔╝██║██╔══██║",
+          " ███████╗██║ ╚████║██║╚██████╔╝██║ ╚═╝ ██║██║  ██║",
+          " ╚══════╝╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝",
+          "",
+          "Crafted By: AJDM Jakiur Rahman",
+          "",
+          "",
+    },
+    center = {
+    {
+      icon = ' ',
+      icon_hl = 'Title',
+      desc = 'Find File           ',
+      desc_hl = 'String',
+      key = 'f',
+      keymap = 'SPC',
+      key_hl = 'Number',
+      key_format = ' %s', -- remove default surrounding `[]`
+      action = 'Telescope Finder'
+    },
+  },
+    footer = {}  --your footer
+  }
+}
